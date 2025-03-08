2316de390213cb9b3af64d4cd237436f9cc1fe2c
0mar99 <omarag9099@gmail.com>
2025-03-08 01:36:17 -0600

    sm8350-common: Update blobs from haydn V816.0.17.0.UKKMIXM
    Signed-off-by: 0mar99 <omarag9099@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -157,7 +157,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2024-11-01
+VENDOR_SECURITY_PATCH := 2025-01-01
 
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
